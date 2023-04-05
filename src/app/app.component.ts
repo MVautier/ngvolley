0e0aebd0f515a132264ae7add25b5e31294bf881
Martial VAUTIER <mvautier@idaia.group>
2023-04-06 01:32:05 +0200

    feat: add admin login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
-import { Component, HostListener, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ThemeService } from './core/services/theme.service';
 import { RouteService } from './core/services/route.services';
 import { Router } from '@angular/router';
 import { environment } from '@env/environment';
 import { MatSidenav } from '@angular/material/sidenav';
 import { LayoutService } from './ui/layout/services/layout.service';
+import { AuthorizeApiService } from './authentication/services/authorize-api.service';
+import { ConnectionInfoService } from './authentication/services/connexion-info.service';
+import { Subject, Subscription, takeUntil } from 'rxjs';
+import { ModalService } from './ui/layout/services/modal.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'colomiers-volley';
+    logged: boolean;
     isDarkTheme: boolean = true;
     isMobile: boolean = false;
     theme = 'dark';
     opened = environment.sidenavOpened;
     fullApp = environment.fullApp;
+    subModal: Subscription;
+    notifier = new Subject<void>();
     @ViewChild('sidenav', { static: true }) sidenav: MatSidenav;
     @HostListener('window:resize', ['$event'])
     onResize(event) {
@@ -28,11 +35,24 @@ export class AppComponent {
 
     constructor(
         private router: Router,
+        private modalService: ModalService,
         private layoutService: LayoutService,
+        private authService: AuthorizeApiService,
+        private connexionInfo: ConnectionInfoService,
         private themeService: ThemeService) { 
             if (window.matchMedia('(max-width: 1025px)').matches) {
                 this.isMobile = true;
             }
+        }
+
+        ngOnInit(): void {
+            this.authService.CheckToken().then(logged => {
+                this.logged = logged;
+                this.connexionInfo.GetOnChangeConnexion().subscribe(token => {
+                    this.logged = this.connexionInfo.IsAuth();
+                    console.log('is logged: ', this.logged);
+                });
+            });
         }
 
     checkWindowSize() {
@@ -48,7 +68,38 @@ export class AppComponent {
     }
 
     goAdmin() {
-        this.router.navigate(['admin']);
+        if (!this.logged) {
+            this.showModalLogin();
+        } else {
+            this.router.navigate(['admin']);
+        }
+        
+    }
+
+    showModalLogin() {
+        this.modalService.open({
+            title: 'Connexion',
+            validateLabel: 'Connexion',
+            cancelLabel: 'Mot de passe oublié',
+            showCancel: true,
+            showValidate: true,
+            size: {
+                width: '',
+                height: ''
+            },
+            component: 'login'
+        });
+        this.modalService.returnData
+            .pipe(takeUntil(this.notifier))
+            .subscribe(result => {
+                if (result) {
+                    this.notifier.next();
+                    this.notifier.complete();
+                }
+                if (result?.data?.id_token) {
+                    this.router.navigate(['admin']);
+                }
+            });
     }
 
     toggleMenu() {
