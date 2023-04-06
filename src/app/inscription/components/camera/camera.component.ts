13c3c91322bbd49f60d215508d6a1d54467842fa
Martial VAUTIER <mvautier@idaia.group>
2023-04-06 22:48:12 +0200

    feat: test canvas size

--- src/app/inscription/components/camera/camera.component.ts
+++ src/app/inscription/components/camera/camera.component.ts
@@ -104,20 +104,22 @@ export class CameraComponent implements OnInit {
         const data = capture.imageAsDataUrl;
         const canvas = document.createElement('canvas');
         const context = canvas.getContext('2d');
-        //const screenMode = window.screen.width > window.screen.height ? 'landscape' : 'portrait';
+        const screenMode = window.screen.width > window.screen.height ? 'landscape' : 'portrait';
         const x = 180; //screenMode === 'landscape' ? 180 : 80; //this.width / 3.5;
         const y = 80; //screenMode === 'landscape' ? 80 : 180; //this.height / 6;
         const w = 280; //this.width / 2.3;
         const h = 340; //this.height / 1.4;
-        canvas.width = w; //screenMode === 'landscape' ? w : h;
-        canvas.height = h; //screenMode === 'landscape' ? h : w;
+        canvas.style.width = w + 'px'; //screenMode === 'landscape' ? w : h;
+        canvas.style.height = h + 'px'; //screenMode === 'landscape' ? h : w;
+        canvas.width = w;
+        canvas.height = h;
         const img = new Image();
         img.width = 640; //this.width;
         img.height = 480; //this.height;
         img.onload = () => {
             context.drawImage(img, x, y, w, h, 0, 0, w, h);
             const newData = canvas.toDataURL();
-            //canvas.remove();
+            canvas.remove();
             resolve(newData);
         };
         img.onerror = () => {
