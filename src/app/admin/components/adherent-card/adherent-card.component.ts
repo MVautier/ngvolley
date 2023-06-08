b66a5297e4dfbb4da7621b2c79eab8e2c668c44d
Martial VAUTIER <mvautier@idaia.group>
2023-06-08 20:20:22 +0200

    feat: add adherent card

--- src/app/admin/components/adherent-card/adherent-card.component.ts
+++ src/app/admin/components/adherent-card/adherent-card.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { AdherentAdminService } from '@app/admin/services/adherent-admin.service';
 import { Adherent } from '@app/core/models/adherent.model';
+import { FileService } from '@app/core/services/file.service';
+import { UtilService } from '@app/core/services/util.service';
 
 @Component({
   selector: 'app-adherent-card',
@@ -11,10 +13,35 @@ import { Adherent } from '@app/core/models/adherent.model';
 export class AdherentCardComponent implements OnInit {
   @Input() adherent: Adherent;
   @Output() hide: EventEmitter<void> = new EventEmitter<void>();
-  constructor(private adherentAdminService: AdherentAdminService) { }
+  photo_default = 'assets/images/user-default.png';
+  photo: string;
+
+  constructor(
+    private adherentAdminService: AdherentAdminService, 
+    private utilService: UtilService,
+    private fileService: FileService) { }
 
   ngOnInit(): void {
-    
+    const photoDoc = this.adherent?.Documents?.find(d => d.type === 'photo');
+    if (photoDoc) {
+        this.adherentAdminService.downloadFile(this.adherent.Uid, photoDoc.filename).then(blob => {
+            this.utilService.blobToDataURL(blob, (result) => {
+                if (result) {
+                    this.photo = result;
+                }
+            });
+          }).catch(err => {
+            
+          });
+    }
+  }
+
+  download(filename: string) {
+    this.adherentAdminService.downloadFile(this.adherent.Uid, filename).then(blob => {
+        this.fileService.download(blob, filename);
+      }).catch(err => {
+        console.error(err);
+      });
   }
 
   returnToListe() {
--- src/app/admin/services/adherent-admin.service.ts
+++ src/app/admin/services/adherent-admin.service.ts
@@ -3,6 +3,8 @@ import { AbstractControl, FormGroup } from '@angular/forms';
 import { AdherentFilter } from '@app/core/models/adherent-filter.model';
 import { Adherent } from '@app/core/models/adherent.model';
 import { WebItem } from '@app/core/models/web-item.model';
+import { HttpDataService } from '@app/core/services/http-data.service';
+import { environment } from '@env/environment';
 import { BehaviorSubject } from 'rxjs';
 
 
@@ -11,8 +13,18 @@ export class AdherentAdminService {
     obsFilter: BehaviorSubject<AdherentFilter> = new BehaviorSubject<AdherentFilter>(new AdherentFilter('id'));
     obsAdherent: BehaviorSubject<Adherent> = new BehaviorSubject<Adherent>(null);
 
-    constructor() {
+    constructor(private http: HttpDataService<any>) {
 
+    }
+
+    downloadFile(uid: string, filename: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.http.getWithOptions(`${environment.apiUrl}Document/download/` + filename + '?uid=' + uid, { responseType: 'blob', withCredentials: true }).then(result => {
+                resolve(result);
+            }).catch(err => {
+                reject(err);
+            });
+        });
     }
 
     setFilter(filter: AdherentFilter) {
